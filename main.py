8755d0d317dec3e5ea1f8c67fd73e1fd455f6c23
sabbir-ul-alam <sabbir.ul.alam.sabbir@gmail.com>
2023-03-24 01:25:39 +0600

    Added path parameter for a new api  /get-student/{student_id}

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+students = {
+    1: {
+        "name" : "Sabbir",
+        "age"  : 28,
+        "class" : "Bsc"
+    }
+}
+
+
 @app.get("/")
 def home():
     return {
         "name": "First Data"
     }
+@app.get("/get-student/{student_id}")
+def get_student(student_id : int):
+    return {
+        json.dumps(students[student_id],indent=4)
+    }
+
